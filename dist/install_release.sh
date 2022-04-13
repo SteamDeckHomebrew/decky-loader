6250fafa6e35277d3f30cb23b11193da18adee6c
marios <marios8543@gmail.com>
2022-04-13 21:50:18 +0300

    Fix release script

--- dist/install_release.sh
+++ dist/install_release.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ "$(whoami)" != "root" ]; then
-    su -c "$0 $*"
-    exit
-fi
-
 echo "Installing Steam Deck Plugin Loader release..."
 
 HOMEBREW_FOLDER=/home/deck/homebrew
@@ -18,26 +13,20 @@ mkdir -p ${HOMEBREW_FOLDER}/plugins
 curl -L https://github.com/SteamDeckHomebrew/PluginLoader/releases/latest/download/PluginLoader --output ${HOMEBREW_FOLDER}/services/PluginLoader
 chmod +x ${HOMEBREW_FOLDER}/services/PluginLoader
 
-systemctl stop plugin_loader 2> /dev/null
-systemctl disable plugin_loader 2> /dev/null
-rm -f /etc/systemd/system/plugin_loader.service
-cat > /etc/systemd/system/plugin_loader.service <<- EOM
+systemctl --user stop plugin_loader 2> /dev/null
+systemctl --user disable plugin_loader 2> /dev/null
+rm -f /home/deck/.config/systemd/user/plugin_loader.service
+cat > /home/deck/.config/systemd/user/plugin_loader.service <<- EOM
 [Unit]
 Description=SteamDeck Plugin Loader
-
 [Service]
 Type=simple
-User=root
-Restart=always
-
 ExecStart=/home/deck/homebrew/services/PluginLoader
 WorkingDirectory=/home/deck/homebrew/services
-
 Environment=PLUGIN_PATH=/home/deck/homebrew/plugins
-
 [Install]
-WantedBy=multi-user.target
+WantedBy=default.target
 EOM
-systemctl daemon-reload
-systemctl start plugin_loader
-systemctl enable plugin_loader
+systemctl --user daemon-reload
+systemctl --user start plugin_loader
+systemctl --user enable plugin_loader

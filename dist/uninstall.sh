9f47ebbb650262ba821d10c5a3787ee6c3f25547
Gabriel Jones <ggppjj@gmail.com>
2022-04-28 13:19:43 -0400

    Update uninstall.sh
    First pass for a version that looks similar to the install scripts

--- dist/uninstall.sh
+++ dist/uninstall.sh
@@ -1,3 +1,17 @@
-sudo rm -rf ~/homebrew
-sudo systemctl disable --now plugin_loader.service
-sudo rm /etc/systemd/system/plugin_loader.service
+#!/bin/sh
+
+echo "Uninstalling Steam Deck Plugin Loader and all plugins..."
+
+HOMEBREW_FOLDER=/home/deck/homebrew
+
+# Disable and remove services
+sudo systemctl disable --now plugin_loader.service > /dev/null
+sudo rm -f /home/deck/.config/systemd/user/plugin_loader.service
+sudo rm -f /etc/systemd/system/plugin_loader.service
+
+# Remove temporary folder if it exists from the install process
+rm -rf /tmp/plugin_loader
+
+# Remove folder structure
+sudo rm -rf ${HOMEBREW_FOLDER}
+

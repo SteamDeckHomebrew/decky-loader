f81142b47589256cdd2fd38a769563fb3e5aa796
AAGaming <aa@bigdumb.gq>
2022-05-21 00:10:42 -0400

    wait for react

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,4 +1,6 @@
 import PluginLoader from './plugin-loader';
+import { sleep } from './utils';
+import { initModules } from './webpack';
 
 declare global {
   interface Window {
@@ -9,8 +11,13 @@ declare global {
 if (window.DeckyPluginLoader) {
   window.DeckyPluginLoader?.dismountAll();
 }
-
-window.DeckyPluginLoader = new PluginLoader();
-setTimeout(async () => {
-  window.DeckyPluginLoader?.loadAllPlugins();
-}, 5000);
+(async () => {
+  while (!window.SP_REACT?.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED) {
+    await sleep(10); // need to wait a little for react
+  }
+  initModules();
+  window.DeckyPluginLoader = new PluginLoader();
+  setTimeout(async () => {
+    window.DeckyPluginLoader?.loadAllPlugins();
+  }, 5000);
+})();

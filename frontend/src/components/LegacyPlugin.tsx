ad1f57795eba57b43579ea5f290278b6de531f90
Jonas Dellinger <jonas@dellinger.dev>
2022-05-26 13:31:18 +0200

    Fix LegacyPlugin

--- frontend/src/components/LegacyPlugin.tsx
+++ frontend/src/components/LegacyPlugin.tsx
@@ -1,21 +1,11 @@
 import { VFC } from 'react';
 
-// class LegacyPlugin extends React.Component {
-//     constructor(props: object) {
-//         super(props);
-//     }
-
-//     render() {
-//         return <iframe style={{ border: 'none', width: '100%', height: '100%' }} src={this.props.url}></iframe>
-//     }
-// }
-
 interface Props {
   url: string;
 }
 
-const LegacyPlugin: VFC<Props> = () => {
-  return <div>LegacyPlugin Hello World</div>;
+const LegacyPlugin: VFC<Props> = ({ url }) => {
+  return <iframe style={{ border: 'none', width: '100%', height: '100%' }} src={url}></iframe>;
 };
 
 export default LegacyPlugin;

6c42661f860f63dbb6ca3f90e612190bdbb34cb3
AAGaming <aa@mail.catvibers.me>
2022-07-03 17:37:39 -0400

    hack: temp hide example plugin

--- frontend/src/components/store/Store.tsx
+++ frontend/src/components/store/Store.tsx
@@ -70,7 +70,7 @@ const StorePage: FC<{}> = () => {
     (async () => {
       const res = await fetch('https://beta.deckbrew.xyz/plugins', { method: 'GET' }).then((r) => r.json());
       console.log(res);
-      setData(res);
+      setData(res.filter((x: StorePlugin) => x.name !== 'Example Plugin'));
     })();
     (async () => {
       const res = await fetch('https://plugins.deckbrew.xyz/get_plugins', { method: 'GET' }).then((r) => r.json());

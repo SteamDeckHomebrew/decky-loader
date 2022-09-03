4e846ea0731284b053a00d1f4d719884294b1085
botato <botatooo@protonmail.com>
2022-09-03 17:16:10 -0400

    use state to merge calls instead of having 2 calls

--- frontend/src/components/settings/pages/general/Uninstall.tsx
+++ frontend/src/components/settings/pages/general/Uninstall.tsx
@@ -1,7 +1,10 @@
 import { DialogButton, Field, Menu, MenuItem, showContextMenu } from 'decky-frontend-lib';
+import { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa';
 
 export default function UninstallSettings() {
+  const [keepPlugins, setKeepPlugins] = useState<boolean>(true);
+
   return (
     <Field
       label="Uninstall Decky"
@@ -9,15 +12,16 @@ export default function UninstallSettings() {
       icon={<FaTrashAlt style={{ display: 'block', fill: '#d92626' }} />}
     >
       <DialogButton
-        onClick={(e: MouseEvent) =>
+        onClick={async (e: MouseEvent) => {
           showContextMenu(
             <Menu label="Do you want to keep your plugins?">
-              <MenuItem onSelected={async () => await window.DeckyPluginLoader._uninstallDecky(true)}>Yes</MenuItem>
-              <MenuItem onSelected={async () => await window.DeckyPluginLoader._uninstallDecky(false)}>No</MenuItem>
+              <MenuItem onSelected={() => setKeepPlugins(true)}>Yes</MenuItem>
+              <MenuItem onSelected={() => setKeepPlugins(false)}>No</MenuItem>
             </Menu>,
             e.currentTarget ?? window,
-          )
-        }
+          );
+          await window.DeckyPluginLoader._uninstallDecky(keepPlugins);
+        }}
         style={{ marginLeft: 'auto' }}
       >
         Uninstall

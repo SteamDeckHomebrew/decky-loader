3329813ed2e9cd701d9dc17911360156e5fa28fd
AAGaming <aa@mail.catvibers.me>
2022-11-19 19:09:10 -0500

    remove useless indent

--- backend/settings.py
+++ backend/settings.py
@@ -12,22 +12,22 @@ class SettingsManager:
         wrong_dir = get_homebrew_path(get_home_path(USER))
         if settings_directory == None:
             settings_directory = path.join(wrong_dir, "settings")
-        
+
         self.path = path.join(settings_directory, name + ".json")
-        
+
         #Create the folder with the correct permission
         if not path.exists(settings_directory):
             mkdir(settings_directory)
             chown(settings_directory, USER, USER)
-        
+
         #Copy all old settings file in the root directory to the correct folder
         for file in listdir(wrong_dir):
             if file.endswith(".json"):
                 rename(path.join(wrong_dir,file),
                        path.join(settings_directory, file)) 
                 self.path = path.join(settings_directory, name + ".json")
-                
-        
+
+
         #If the owner of the settings directory is not the user, then set it as the user:
         if get_user_owner(settings_directory) != USER:
             chown(settings_directory, USER, USER)

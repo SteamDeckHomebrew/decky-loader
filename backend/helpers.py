4319286a03932781110a417a0fbdcca1fcabe8d2
Philipp Richter <richterphilipp.pops@gmail.com>
2023-01-23 15:16:48 +0100

    Expose a 'decky_plugin' module to decky plugins
    * expose decky user home path
    * support 'py_modules' python modules in plugins
    * allow for a '_migration' method in plugins to have an explicit file
      moving step

--- backend/helpers.py
+++ backend/helpers.py
@@ -115,8 +115,11 @@ def mkdir_as_user(path):
 
 # Fetches the version of loader
 def get_loader_version() -> str:
-    with open(os.path.join(os.path.dirname(sys.argv[0]), ".loader.version"), "r", encoding="utf-8") as version_file:
-        return version_file.readline().replace("\n", "")
+    try:
+        with open(os.path.join(os.path.dirname(sys.argv[0]), ".loader.version"), "r", encoding="utf-8") as version_file:
+            return version_file.readline().replace("\n", "")
+    except:
+        return ""
 
 # Download Remote Binaries to local Plugin
 async def download_remote_binary_to_path(url, binHash, path) -> bool:

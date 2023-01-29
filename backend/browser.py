36cf85b08a15a8e55b6931894bf448b2977711e5
TrainDoctor <traindoctor@protonmail.com>
2023-01-29 15:27:52 -0800

    Comment out un-needed pprint usage

--- backend/browser.py
+++ backend/browser.py
@@ -1,7 +1,7 @@
 # Full imports
 import json
-import pprint
-from pprint import pformat
+# import pprint
+# from pprint import pformat
 
 # Partial imports
 from aiohttp import ClientSession, web
@@ -111,10 +111,11 @@ class PluginBrowser:
             logger.info("uninstalling " + name)
             logger.info(" at dir " + self.find_plugin_folder(name))
             logger.debug("calling frontend unload for %s" % str(name))
-            await tab.evaluate_js(f"DeckyPluginLoader.unloadPlugin('{name}')")
-            plugins_snapshot = self.plugins.copy()
-            snapshot_string = pformat(plugins_snapshot)
-            logger.debug("current plugins: %s", snapshot_string)
+            res = await tab.evaluate_js(f"DeckyPluginLoader.unloadPlugin('{name}')")
+            logger.debug("result of unload from UI: %s", res)
+            # plugins_snapshot = self.plugins.copy()
+            # snapshot_string = pformat(plugins_snapshot)
+            # logger.debug("current plugins: %s", snapshot_string)
             if self.plugins[name]:
                 logger.debug("Plugin %s was found", name)
                 self.plugins[name].stop()

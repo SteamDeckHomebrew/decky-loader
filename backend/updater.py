eba5dcfdf3f29ed84a5c70fe7483955cd7e8c1e0
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2023-02-14 00:02:31 +0100

    Fix branch guess from version

--- backend/updater.py
+++ backend/updater.py
@@ -68,7 +68,7 @@ class Updater:
         logger.debug("current branch: %i" % ver)
         if ver == -1:
             logger.info("Current branch is not set, determining branch from version...")
-            if self.localVer.startswith("v") and self.localVer.find("-pre"):
+            if self.localVer.startswith("v") and self.localVer.find("-pre") != -1:
                 logger.info("Current version determined to be pre-release")
                 return 1
             else:

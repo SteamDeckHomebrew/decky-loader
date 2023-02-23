f9624a0859107e781312a4e10453647ea8029888
AAGaming <aa@mail.catvibers.me>
2023-02-22 22:03:19 -0500

    how did this ever happen

--- frontend/src/utils/windows.ts
+++
@@ -1,9 +0,0 @@
-import { getFocusNavController } from '../../../../lib/dist';
-
-export function findSP(): Window {
-  // old (SP as host)
-  if (document.title == 'SP') return window;
-  // new (SP as popup)
-  return getFocusNavController().m_ActiveContext.m_rgGamepadNavigationTrees.find((x: any) => x.m_ID == 'root_1_').Root
-    .Element.ownerDocument.defaultView;
-}

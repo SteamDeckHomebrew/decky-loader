4a97984cbede8d3b52f5d83c24efdd8256f8b862
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-04-04 11:45:45 +0200

    Messed up deletion of rollup config.

---
+++ frontend/rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from '@rollup/plugin-commonjs';
+import image from '@rollup/plugin-image';
+import json from '@rollup/plugin-json';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import { defineConfig } from 'rollup';
+import del from 'rollup-plugin-delete';
+import externalGlobals from 'rollup-plugin-external-globals';
+
+const hiddenWarnings = ['THIS_IS_UNDEFINED', 'EVAL'];
+
+export default defineConfig({
+  input: 'src/index.tsx',
+  plugins: [
+    del({ targets: '../backend/static/*', force: true }),
+    commonjs({include: '../frontend/node_modules/**'}),
+    nodeResolve({
+      browser: true
+    }),
+    externalGlobals({
+      react: 'SP_REACT',
+      'react-dom': 'SP_REACTDOM',
+      // hack to shut up react-markdown
+      process: '{cwd: () => {}}',
+      path: '{dirname: () => {}, join: () => {}, basename: () => {}, extname: () => {}}',
+      url: '{fileURLToPath: (f) => f}',
+    }),
+    typescript(),
+    json(),
+    replace({
+      preventAssignment: false,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    image(),
+  ],
+  preserveEntrySignatures: false,
+  output: {
+    dir: '../backend/static',
+    format: 'esm',
+    chunkFileNames: (chunkInfo) => {
+      return 'chunk-[hash].js';
+    },
+  },
+  onwarn: function (message, handleWarning) {
+    if (hiddenWarnings.some((warning) => message.code === warning)) return;
+    handleWarning(message);
+  },
+});

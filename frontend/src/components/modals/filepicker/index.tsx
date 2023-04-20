61c82768a480516da25960f207d34b31335a93c6
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-04-20 19:52:13 +0200

    First iteration for merging the new filepicker.

--- frontend/src/components/modals/filepicker/index.tsx
+++ frontend/src/components/modals/filepicker/index.tsx
@@ -1,27 +1,45 @@
-import { DialogButton, Focusable, SteamSpinner, TextField } from 'decky-frontend-lib';
-import { useEffect } from 'react';
-import { FunctionComponent, useState } from 'react';
+import {
+  ControlsList,
+  DialogBody,
+  DialogButton,
+  DialogControlsSection,
+  DialogFooter,
+  Dropdown,
+  Focusable,
+  Marquee,
+  ServerAPI,
+  SteamSpinner,
+  TextField,
+  ToggleField,
+} from 'decky-frontend-lib';
+import { filesize } from 'filesize';
+import { FunctionComponent, useCallback, useEffect, useMemo, useState } from 'react';
 import { FileIcon, defaultStyles } from 'react-file-icon';
 import { useTranslation } from 'react-i18next';
 import { FaArrowUp, FaFolder } from 'react-icons/fa';
 
-import Logger from '../../../logger';
+import DropdownMultiselect from '../DropdownMultiselect';
 import { styleDefObj } from './iconCustomizations';
 
-const logger = new Logger('FilePicker');
-
 export interface FilePickerProps {
+  serverApi: ServerAPI;
   startPath: string;
   includeFiles?: boolean;
-  regex?: RegExp;
+  filter?: RegExp | ((file: File) => boolean);
+  validFileExtensions?: string[];
+  defaultHidden?: boolean;
   onSubmit: (val: { path: string; realpath: string }) => void;
   closeModal?: () => void;
 }
 
-interface File {
+export interface File {
   isdir: boolean;
+  ishidden: boolean;
   name: string;
   realpath: string;
+  size: number;
+  modified: number;
+  created: number;
 }
 
 interface FileListing {
@@ -29,12 +47,70 @@ interface FileListing {
   files: File[];
 }
 
+type SortOption =
+  | 'name_desc'
+  | 'name_asc'
+  | 'modified_desc'
+  | 'modified_asc'
+  | 'created_desc'
+  | 'created_asc'
+  | 'size_desc'
+  | 'size_asc';
+
 function getList(
+  serverApi: ServerAPI,
   path: string,
-  includeFiles: boolean = true,
+  includeFiles = true,
 ): Promise<{ result: FileListing | string; success: boolean }> {
-  return window.DeckyPluginLoader.callServerMethod('filepicker_ls', { path, include_files: includeFiles });
+  return serverApi.callPluginMethod('filepicker_new', { path, include_files: includeFiles });
 }
+
+const sortOptions = [
+  {
+    data: 'name_desc',
+    label: () => {
+      const { t } = useTranslation();
+      return t('FilePickerIndex.filter.name_desc');
+    },
+  },
+  {
+    data: 'name_asc',
+    label: () => {
+      const { t } = useTranslation();
+      return t('FilePickerIndex.filter.name_asce');
+    },
+  },
+  {
+    data: 'modified_desc',
+    label: () => {
+      const { t } = useTranslation();
+      return t('FilePickerIndex.filter.modified_desc');
+    },
+  },
+  {
+    data: 'modified_asc',
+    label: () => {
+      const { t } = useTranslation();
+      return t('FilePickerIndex.filter.modified_asce');
+    },
+  },
+  {
+    data: 'created_desc',
+    label: 'Created (Newest)',
+  },
+  {
+    data: 'created_asc',
+    label: 'Created (Oldest)',
+  },
+  {
+    data: 'size_desc',
+    label: 'Size (Largest)',
+  },
+  {
+    data: 'size_asc',
+    label: 'Size (Smallest)',
+  },
+];
 
 const iconStyles = {
   paddingRight: '10px',
@@ -42,120 +118,220 @@ const iconStyles = {
 };
 
 const FilePicker: FunctionComponent<FilePickerProps> = ({
+  serverApi,
   startPath,
   includeFiles = true,
-  regex,
+  filter,
+  validFileExtensions = null,
+  defaultHidden = false, // false by default makes sense for most users
   onSubmit,
   closeModal,
 }) => {
   const { t } = useTranslation();
-  if (startPath.endsWith('/')) startPath = startPath.substring(0, startPath.length - 1); // remove trailing path
+
+  if (startPath !== '/' && startPath.endsWith('/')) startPath = startPath.substring(0, startPath.length - 1); // remove trailing path
   const [path, setPath] = useState<string>(startPath);
   const [listing, setListing] = useState<FileListing>({ files: [], realpath: path });
+  const [files, setFiles] = useState<File[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [showHidden, setShowHidden] = useState<boolean>(defaultHidden);
+  const [sort, setSort] = useState<SortOption>('name_desc');
+  const [selectedFiles, setSelectedFiles] = useState<any>(validFileExtensions);
+
+  const validExtsOptions = useMemo(() => {
+    if (!validFileExtensions) return [];
+    return [{ label: 'All Files', value: 'all_files' }, ...validFileExtensions.map((x) => ({ label: x, value: x }))];
+  }, [validFileExtensions]);
+
+  const handleExtsSelect = useCallback((val: any) => {
+    // unselect other options if "All Files" is checked
+    if (val.includes('all_files')) {
+      setSelectedFiles(['all_files']);
+    } else {
+      setSelectedFiles(val);
+    }
+  }, []);
 
   useEffect(() => {
     (async () => {
-      if (error) setError(null);
       setLoading(true);
-      const listing = await getList(path, includeFiles);
+      const listing = await getList(serverApi, path, includeFiles);
       if (!listing.success) {
         setListing({ files: [], realpath: path });
         setLoading(false);
         setError(listing.result as string);
-        logger.error(listing.result);
         return;
+      } else {
+        setError(null);
+        setFiles((listing.result as FileListing).files);
       }
       setLoading(false);
       setListing(listing.result as FileListing);
-      logger.log('reloaded', path, listing);
     })();
-  }, [path]);
+  }, [error, includeFiles, path, serverApi]);
+
+  useEffect(() => {
+    const files = [...listing.files]
+      // Hidden files filter
+      .filter((file) => {
+        if (showHidden && file.ishidden) return true;
+        if (!showHidden && file.ishidden) return false;
+        return true;
+      })
+      // File extension filter
+      .filter((file) => {
+        if (!validFileExtensions || file.isdir || selectedFiles.includes('all_files')) return true;
+
+        const extension = file.realpath.split('.').pop() as string;
+        if (selectedFiles.includes(extension)) return true;
+        return false;
+      })
+      // Custom filter
+      .filter((file) => {
+        if (filter instanceof RegExp) return filter.test(file.name);
+        if (typeof filter === 'function') return filter(file);
+        return true;
+      })
+      // Sort files
+      .sort((a, b) => {
+        const key = sort.split('_')[0];
+        const order = sort.split('_')[1];
+        if (key === 'name') {
+          return order === 'asc' ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name);
+        }
+        return order === 'asc' ? (a[key] > b[key] ? 1 : -1) : b[key] > a[key] ? 1 : -1;
+      })
+      // Put directories before files
+      .reduceRight((acc, file) => (file.isdir ? [file, ...acc] : [...acc, file]), [] as File[]);
+    setFiles(files);
+  }, [listing.files, filter, showHidden, sort, selectedFiles, validFileExtensions]);
 
   return (
-    <div className="deckyFilePicker">
-      <Focusable style={{ display: 'flex', flexDirection: 'row', paddingBottom: '10px' }}>
-        <DialogButton
-          style={{
-            minWidth: 'unset',
-            width: '40px',
-            flexGrow: '0',
-            borderRadius: 'unset',
-            margin: '0',
-            padding: '10px',
-          }}
-          onClick={() => {
-            const newPathArr = path.split('/');
-            newPathArr.pop();
-            let newPath = newPathArr.join('/');
-            if (newPath == '') newPath = '/';
-            setPath(newPath);
-          }}
-        >
-          <FaArrowUp />
-        </DialogButton>
-        <div style={{ flexGrow: '1', width: '100%' }}>
-          <TextField
-            value={path}
-            onChange={(e) => {
-              e.target.value && setPath(e.target.value);
-            }}
-            style={{ height: '100%' }}
-          />
-        </div>
-      </Focusable>
-      <Focusable style={{ display: 'flex', flexDirection: 'column', height: '60vh', overflow: 'scroll' }}>
-        {loading && <SteamSpinner style={{ height: '100%' }} />}
-        {!loading &&
-          listing.files
-            .filter((file) => (includeFiles || file.isdir) && (!regex || regex.test(file.name)))
-            .map((file) => {
-              let extension = file.realpath.split('.').pop() as string;
-              return (
-                <DialogButton
-                  style={{ borderRadius: 'unset', margin: '0', padding: '10px' }}
-                  onClick={() => {
-                    const fullPath = `${path}${path.endsWith('/') ? '' : '/'}${file.name}`;
-                    if (file.isdir) setPath(fullPath);
-                    else {
-                      onSubmit({ path: fullPath, realpath: file.realpath });
-                      closeModal?.();
-                    }
-                  }}
-                >
-                  <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
-                    {file.isdir ? (
-                      <FaFolder style={iconStyles} />
-                    ) : (
-                      <div style={iconStyles}>
-                        {file.realpath.includes('.') ? (
-                          <FileIcon {...defaultStyles[extension]} {...styleDefObj[extension]} extension={''} />
-                        ) : (
-                          <FileIcon />
-                        )}
-                      </div>
-                    )}
-                    {file.name}
-                  </div>
-                </DialogButton>
-              );
-            })}
-        {error}
-      </Focusable>
+    <>
+      <DialogBody>
+        <DialogControlsSection>
+          <Focusable flow-children="right" style={{ display: 'flex', marginBottom: '1em' }}>
+            <DialogButton
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minWidth: 'unset',
+                width: '40px',
+                borderRadius: 'unset',
+                margin: '0',
+                padding: '10px',
+              }}
+              onClick={() => {
+                const newPathArr = path.split('/');
+                newPathArr.pop();
+                let newPath = newPathArr.join('/');
+                if (newPath == '') newPath = '/';
+                setPath(newPath);
+              }}
+            >
+              <FaArrowUp />
+            </DialogButton>
+            <div style={{ width: '100%' }}>
+              <TextField
+                value={path}
+                onChange={(e) => {
+                  e.target.value && setPath(e.target.value);
+                }}
+                style={{ height: '100%' }}
+              />
+            </div>
+          </Focusable>
+          <ControlsList alignItems="center" spacing="standard">
+            <ToggleField
+              highlightOnFocus={false}
+              label="Show Hidden Files"
+              bottomSeparator="none"
+              checked={showHidden}
+              onChange={() => setShowHidden((x) => !x)}
+            />
+            <Dropdown rgOptions={sortOptions} selectedOption={sort} onChange={(x) => setSort(x.data)} />
+            {validFileExtensions && (
+              <DropdownMultiselect
+                label="File Type"
+                items={validExtsOptions}
+                selected={selectedFiles}
+                onSelect={handleExtsSelect}
+              />
+            )}
+          </ControlsList>
+        </DialogControlsSection>
+        <DialogControlsSection style={{ marginTop: '1em' }}>
+          <Focusable
+            style={{ display: 'flex', gap: '.25em', flexDirection: 'column', height: '60vh', overflow: 'scroll' }}
+          >
+            {loading && <SteamSpinner style={{ height: '100%' }} />}
+            {!loading &&
+              files.map((file) => {
+                const extension = file.realpath.split('.').pop() as string;
+                return (
+                  <DialogButton
+                    key={`${file.realpath}${file.name}`}
+                    style={{ borderRadius: 'unset', margin: '0', padding: '10px' }}
+                    onClick={() => {
+                      const fullPath = `${path}${path.endsWith('/') ? '' : '/'}${file.name}`;
+                      if (file.isdir) setPath(fullPath);
+                      else {
+                        onSubmit({ path: fullPath, realpath: file.realpath });
+                        closeModal?.();
+                      }
+                    }}
+                  >
+                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start' }}>
+                      {file.isdir ? (
+                        <FaFolder style={iconStyles} />
+                      ) : (
+                        <div style={iconStyles}>
+                          {file.realpath.includes('.') ? (
+                            <FileIcon {...defaultStyles[extension]} {...styleDefObj[extension]} extension={''} />
+                          ) : (
+                            <FileIcon />
+                          )}
+                        </div>
+                      )}
+                      <Marquee>{file.name}</Marquee>
+                    </div>
+                    <div
+                      style={{
+                        display: 'flex',
+                        opacity: 0.5,
+                        fontSize: '.6em',
+                        textAlign: 'left',
+                        lineHeight: 1,
+                        marginTop: '.5em',
+                      }}
+                    >
+                      {file.isdir ? 'Folder' : filesize(file.size, { standard: 'iec' })}
+                      <span style={{ marginLeft: 'auto' }}>{new Date(file.modified * 1000).toLocaleString()}</span>
+                    </div>
+                  </DialogButton>
+                );
+              })}
+            {error}
+          </Focusable>
+        </DialogControlsSection>
+      </DialogBody>
       {!loading && !error && !includeFiles && (
-        <DialogButton
-          className="Primary"
-          style={{ marginTop: '10px', alignSelf: 'flex-end' }}
-          onClick={() => {
-            onSubmit({ path, realpath: listing.realpath });
-            closeModal?.();
-          }}
-        >
-          {t('FilePickerIndex.folder.select')}
-        </DialogButton>
+        <DialogFooter>
+          <DialogButton
+            className="Primary"
+            style={{ marginTop: '10px', alignSelf: 'flex-end' }}
+            onClick={() => {
+              onSubmit({ path, realpath: listing.realpath });
+              closeModal?.();
+            }}
+          >
+            {t('FilePickerIndex.folder.select')}
+          </DialogButton>
+        </DialogFooter>
       )}
-    </div>
+    </>
   );
 };
 

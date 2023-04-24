473358adbcf042c850d436e526eb8e8d9a5b56ff
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-04-24 09:18:10 +0200

    Reimplemented this component as a functional component.

--- frontend/src/components/modals/TPluginInstallModal.tsx
+++ frontend/src/components/modals/TPluginInstallModal.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Translation } from 'react-i18next';
 
 import { InstallType } from '../../plugin';
@@ -9,93 +9,87 @@ export enum TranslatedPart {
   BUTTON_IDLE,
   BUTTON_PROC,
 }
-class TPluginInstallModal extends Component<TranslationProps> {
-  public static defaultProps = {
-    artifact: '',
-    version: '',
-  };
-  render() {
-    return (
-      <Translation>
-        {(t, {}) => {
-          switch (this.props.trans_part) {
-            case TranslatedPart.TITLE:
-              switch (this.props.trans_type) {
-                case InstallType.INSTALL:
-                  return <div>{t('PluginInstallModal.install.title', { artifact: this.props.artifact })}</div>;
-                case InstallType.REINSTALL:
-                  return <div>{t('PluginInstallModal.reinstall.title', { artifact: this.props.artifact })}</div>;
-                case InstallType.UPDATE:
-                  return <div>{t('PluginInstallModal.update.title', { artifact: this.props.artifact })}</div>;
-                default:
-                  return null;
-              }
-            case TranslatedPart.DESC:
-              switch (this.props.trans_type) {
-                case InstallType.INSTALL:
-                  return (
-                    <div>
-                      {t('PluginInstallModal.install.desc', {
-                        artifact: this.props.artifact,
-                        version: this.props.version,
-                      })}
-                    </div>
-                  );
-                case InstallType.REINSTALL:
-                  return (
-                    <div>
-                      {t('PluginInstallModal.reinstall.desc', {
-                        artifact: this.props.artifact,
-                        version: this.props.version,
-                      })}
-                    </div>
-                  );
-                case InstallType.UPDATE:
-                  return (
-                    <div>
-                      {t('PluginInstallModal.update.desc', {
-                        artifact: this.props.artifact,
-                        version: this.props.version,
-                      })}
-                    </div>
-                  );
-                default:
-                  return null;
-              }
-            case TranslatedPart.BUTTON_IDLE:
-              switch (this.props.trans_type) {
-                case InstallType.INSTALL:
-                  return <div>{t('PluginInstallModal.install.button_idle')}</div>;
-                case InstallType.REINSTALL:
-                  return <div>{t('PluginInstallModal.reinstall.button_idle')}</div>;
-                case InstallType.UPDATE:
-                  return <div>{t('PluginInstallModal.update.button_idle')}</div>;
-                default:
-                  return null;
-              }
-            case TranslatedPart.BUTTON_PROC:
-              switch (this.props.trans_type) {
-                case InstallType.INSTALL:
-                  return <div>{t('PluginInstallModal.install.button_processing')}</div>;
-                case InstallType.REINSTALL:
-                  return <div>{t('PluginInstallModal.reinstall.button_processing')}</div>;
-                case InstallType.UPDATE:
-                  return <div>{t('PluginInstallModal.update.button_processing')}</div>;
-                default:
-                  return null;
-              }
-          }
-        }}
-      </Translation>
-    );
-  }
-}
-
-interface TranslationProps {
+interface TPluginInstallModalProps {
   trans_part: TranslatedPart;
   trans_type: number;
-  artifact: string;
-  version: string;
+  artifact?: string;
+  version?: string;
 }
+
+const TPluginInstallModal: FC<TPluginInstallModalProps> = ({ trans_part, trans_type, artifact, version }) => {
+  return (
+    <Translation>
+      {(t, {}) => {
+        switch (trans_part) {
+          case TranslatedPart.TITLE:
+            switch (trans_type) {
+              case InstallType.INSTALL:
+                return <div>{t('PluginInstallModal.install.title', { artifact: artifact })}</div>;
+              case InstallType.REINSTALL:
+                return <div>{t('PluginInstallModal.reinstall.title', { artifact: artifact })}</div>;
+              case InstallType.UPDATE:
+                return <div>{t('PluginInstallModal.update.title', { artifact: artifact })}</div>;
+              default:
+                return null;
+            }
+          case TranslatedPart.DESC:
+            switch (trans_type) {
+              case InstallType.INSTALL:
+                return (
+                  <div>
+                    {t('PluginInstallModal.install.desc', {
+                      artifact: artifact,
+                      version: version,
+                    })}
+                  </div>
+                );
+              case InstallType.REINSTALL:
+                return (
+                  <div>
+                    {t('PluginInstallModal.reinstall.desc', {
+                      artifact: artifact,
+                      version: version,
+                    })}
+                  </div>
+                );
+              case InstallType.UPDATE:
+                return (
+                  <div>
+                    {t('PluginInstallModal.update.desc', {
+                      artifact: artifact,
+                      version: version,
+                    })}
+                  </div>
+                );
+              default:
+                return null;
+            }
+          case TranslatedPart.BUTTON_IDLE:
+            switch (trans_type) {
+              case InstallType.INSTALL:
+                return <div>{t('PluginInstallModal.install.button_idle')}</div>;
+              case InstallType.REINSTALL:
+                return <div>{t('PluginInstallModal.reinstall.button_idle')}</div>;
+              case InstallType.UPDATE:
+                return <div>{t('PluginInstallModal.update.button_idle')}</div>;
+              default:
+                return null;
+            }
+          case TranslatedPart.BUTTON_PROC:
+            switch (trans_type) {
+              case InstallType.INSTALL:
+                return <div>{t('PluginInstallModal.install.button_processing')}</div>;
+              case InstallType.REINSTALL:
+                return <div>{t('PluginInstallModal.reinstall.button_processing')}</div>;
+              case InstallType.UPDATE:
+                return <div>{t('PluginInstallModal.update.button_processing')}</div>;
+              default:
+                return null;
+            }
+        }
+      }}
+    </Translation>
+  );
+};
 
 export default TPluginInstallModal;

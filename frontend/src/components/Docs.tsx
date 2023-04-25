3a0bfc7e7c87672bc20c59217062dfce5a64806d
Party Wumpus <48649272+PartyWumpus@users.noreply.github.com>
2023-04-25 20:28:37 +0100

    give credit

--- frontend/src/components/Docs.tsx
+++ frontend/src/components/Docs.tsx
@@ -1,3 +1,5 @@
+// this file was mostly (the good bits) made by davocarli#7308
+// it should probably be moved to DFL eventually
 import { VFC, useEffect, useState } from 'react';
 import { useParams, SidebarNavigation, SteamSpinner, Focusable } from "decky-frontend-lib";
 import { lazy } from 'react';

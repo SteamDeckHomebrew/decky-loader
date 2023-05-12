66c741262da8a0b21e372b720f8a600e11ebfaaf
AAGaming <aa@mail.catvibers.me>
2023-05-11 21:50:21 -0400

    i forgor another comment

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,7 +2,5 @@
 (async () => {
   console.debug('Setting up decky-frontend-lib...');
   window.DFL = await import('decky-frontend-lib');
-  console.debug('decky-frontend-lib ready!');
-  console.debug('window.DFL: ', window.DFL);
   await import('./start');
 })();

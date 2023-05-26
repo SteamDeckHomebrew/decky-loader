dfdad14ede06a2287ab47fd842b136a75160725a
Marco Rodolfi <marco.rodolfi@tuta.io>
2023-05-26 13:29:47 +0200

    Fix: actually force the cog icon to stay inline

--- frontend/src/plugin-loader.tsx
+++ frontend/src/plugin-loader.tsx
@@ -1,6 +1,7 @@
 import { ConfirmModal, ModalRoot, Patch, QuickAccessTab, Router, showModal, sleep } from 'decky-frontend-lib';
 import { FC, lazy } from 'react';
 import { Trans, Translation } from 'react-i18next';
+import { IconContext } from 'react-icons';
 import { FaCog, FaExclamationCircle, FaPlug } from 'react-icons/fa';
 
 import { DeckyState, DeckyStateContextProvider, useDeckyState } from './components/DeckyState';
@@ -265,12 +266,11 @@ class PluginLoader extends Logger {
                   <pre>
                     <code>{e instanceof Error ? e.stack : JSON.stringify(e)}</code>
                   </pre>
-                  <>
-                    <Trans
-                      i18nKey="PluginLoader.plugin_error_uninstall"
-                      components={[<FaCog style={{ display: 'inline' }} />]}
-                    />
-                  </>
+                  <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+                    <div>
+                      <Trans i18nKey="PluginLoader.plugin_error_uninstall" components={[<FaCog />]} />
+                    </div>
+                  </IconContext.Provider>
                 </>
               );
             }}

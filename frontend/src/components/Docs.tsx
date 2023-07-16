e4b0bb6ff352519cbc4e7d59d6bd7cfead96589e
Party Wumpus <48649272+PartyWumpus@users.noreply.github.com>
2023-07-16 17:05:09 +0100

    "borrow" tabmaster's css

--- frontend/src/components/Docs.tsx
+++ frontend/src/components/Docs.tsx
@@ -13,14 +13,21 @@ const DocsPage: VFC<{ content: string }> = ({ content }) => {
       <>
         <style>
         {`
-          .deckyDocsMarkdown p {white-space: pre-wrap}
-          .deckyDocsMarkdown code {background: #9999990f;}
-          .deckyDocsMarkdown a {text-decoration: none;}
+        .decky-docs-markdown p {white-space: pre-wrap}
+        .decky-docs-markdown a {text-decoration: none;}
+        .decky-docs-markdown code {color: #f1ac4f; padding: 2px 4px; border-radius: 4px;}
+        .decky-docs-markdown table {border: 1px solid; border-collapse: collapse;}
+        .decky-docs-markdown th {padding: 0 7px; border: 1px solid;}
+        .decky-docs-markdown td {padding: 0 7px; border: 1px solid;}
+        .decky-docs-markdown tr:nth-child(odd) {background-color: #1B2838;}
+        .decky-docs-markdown > .Panel.Focusable.gpfocuswithin {background-color: #868da117;}
+        .decky-docs-markdown img {max-width: 588px;}
+        `}
         `}
         </style>
         <Scrollable ref={ref}>
             <ScrollArea scrollable={ref} noFocusRing={true}>
-              <MarkdownRenderer className="deckyDocsMarkdown" children={content} />
+              <MarkdownRenderer className="decky-docs-markdown" children={content} />
           </ScrollArea>
         </Scrollable>
       </>

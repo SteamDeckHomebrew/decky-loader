83f2c947122caa8e1d215dbd42def42ec787137a
Sims <38142618+suchmememanyskill@users.noreply.github.com>
2024-12-14 04:38:33 +0100

    Fix id keyerror on plugin uninstall (#725)
    * Fix id keyerror on plugin uninstall
    
    * This is python
    
    * too bad im changing it anyway
    
    ---------
    
    Co-authored-by: AAGaming <aagaming@riseup.net>

--- backend/decky_loader/plugin/sandboxed_plugin.py
+++ backend/decky_loader/plugin/sandboxed_plugin.py
@@ -186,6 +186,7 @@ class SandboxedPlugin:
 
         if "uninstall" in data:
             self.uninstalling = data.get("uninstall")
+            return
 
         d: SocketResponseDict = {"type": SocketMessageType.RESPONSE, "res": None, "success": True, "id": data["id"]}
         try:

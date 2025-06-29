b30c495e06be6c63b3a1b65b97f7764a2a8c3367
TrainDoctor <traindoctor@protonmail.com>
2025-06-29 11:23:46 -0700

    Lint fixup

--- frontend/src/components/DeckyErrorBoundary.tsx
+++ frontend/src/components/DeckyErrorBoundary.tsx
@@ -172,7 +172,7 @@ const DeckyErrorBoundary: FunctionComponent<DeckyErrorBoundaryProps> = ({ error,
                 </button>
               </div>
             )}
-            {(
+            {
               <div style={{ display: 'block', marginBottom: '5px' }}>
                 {updateProgress > -1
                   ? 'Update in progress... ' + updateProgress + '%'
@@ -230,7 +230,7 @@ const DeckyErrorBoundary: FunctionComponent<DeckyErrorBoundaryProps> = ({ error,
                   </div>
                 )}
               </div>
-            )}
+            }
             {wasCausedByPlugin && (
               <div style={{ display: 'block', marginBottom: '5px' }}>
                 {'\n'}
